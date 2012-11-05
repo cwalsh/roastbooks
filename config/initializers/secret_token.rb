29eeccc075f9bcbca528ea2a1b21747fe84f25aa
Cameron Walsh <cameron.walsh@gmail.com>
2012-11-05 12:40:18 +1100

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roastbooks::Application.config.secret_token = 'd53426578b3e1fb034caf8a481251381188cb00d7793e874eb7cc1ec6b94ad226f6c6d6c7d82c329cdba8adf0d1101045c7b3ccd874e735fd76dc0d3da3a54a3'
